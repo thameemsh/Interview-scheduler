1fba889185b906446dfc2fe2320f723f430a2893
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-14 17:16:31 -0500

    Updating spots count to DB at the time of creating, editing or deleting

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,28 +1,41 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-
-  const [state, setState] = useState ({
-    day : "Monday",
-    days : [],
-    appointments : {},
-    interviewers : {}
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
   });
- 
- 
-  const setDay = day => setState({ ...state, day });
+
+  const setDay = (day) => setState({ ...state, day });
 
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("http://localhost:8001/api/interviewers"),
     ]).then((all) => {
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
-    })
-  }, [])
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, []);
 
+  function dayOfTheWeek(day) {
+    const dayOfWeek = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4,
+    };
+    return dayOfWeek[day];
+  }
 
   function bookInterview(id, interview) {
     // console.log(id, interview);
@@ -34,25 +47,64 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+
+    const dayOfWeek = dayOfTheWeek(state.day);
+
+    let day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek],
+    };
+
+
+    if (!state.appointments[id].interview) {
+      day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spots - 1,
+      };
+    } else { //When Editing the count must not change
+      day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spots,
+      };
+    }
+
+    let days = state.days;
+    days[dayOfWeek] = day;
+
+   
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments: appointments }));
+      .then(() => {
+        setState({ ...state, appointments: appointments, days: days });
+      });
   }
-
 
   function cancelInterview(id) {
     const appointment = {
-     ...state.appointments[id],
-     interview: null
-   };
-   const appointments = {
-     ...state.appointments,
-     [id]: appointment
-   };
-   return axios.delete(`http://localhost:8001/api/appointments/${id}`)
-   .then(() => setState({...state, appointments:appointments}) )
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
 
- }
+    const dayOfWeek = dayOfTheWeek(state.day);
+
+    const day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek].spots + 1,
+    };
+
+    let days = state.days;
+    days[dayOfWeek] = day;
+
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() =>
+        setState({ ...state, appointments: appointments, days: days })
+      );
+  }
 
   return { state, setDay, bookInterview, cancelInterview };
 }
