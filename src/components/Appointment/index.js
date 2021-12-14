85311404ea538fb01f4f79f3cafcdb429289e9ac
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-14 07:28:49 -0500

    Show loading while fetching data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 import "components/Appointment/styles.scss";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
@@ -9,6 +10,7 @@ import Form from "./Form";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 export default function Appointment(props) {
 
   const { mode, transition, back } = useVisualMode(
@@ -19,19 +21,25 @@ export default function Appointment(props) {
         student: name,
         interviewer
       };
+
       const bookInterviewPromise = () => {
         return new Promise((resolve) => {
           props.bookInterview(props.id, interview)
           resolve()
       })
     }
-      bookInterviewPromise().then(() => transition(SHOW))
+      bookInterviewPromise().then(() => {
+        setTimeout(() =>transition(SAVING),50);
+        setTimeout(() =>transition(SHOW),750);
+      })
+
     }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message="Saving" /> } 
       {mode === CREATE && (
         <Form
           interviewers = {props.interviewers}
