8b3e83b3ef554ad9ee9fc348f0ca019b8bc24dde
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-15 13:25:43 -0500

    Setting the condition that the prop-types needs to an Array for InterviewList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
 
 
   const interviewListItemsArray = props.interviewers.map((interviewer) => 
@@ -23,3 +24,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
