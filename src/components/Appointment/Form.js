f54253b42175bcdf22d271e46b3ba268cdb0911d
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-15 16:01:18 -0500

    Add validation on selecting interviwer in Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -28,6 +28,11 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
     setError("");
     props.onSave(student, interviewer);
   }
