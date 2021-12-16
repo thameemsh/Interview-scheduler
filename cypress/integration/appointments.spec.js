d870de0a3d70228ffecb80cc11a9d83e75ecfd5c
Mohamed Shahul Hameed <thameemsh@email.com>
2021-12-16 10:03:44 -0500

    Adding Cypress test for booking an interview and editing an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,42 @@
-describe('Appointments', () => {
-
+describe("Appointments", () => {
+  beforeEach(() => {
+   cy.request("GET", "/api/debug/reset");
+ 
+   cy.visit("/");
+ 
+   cy.contains("Monday");
+  });
+ 
   it("should book an interview", () => {
-    cy.visit("/");
-    cy.contains("Monday");
+   cy.get("[alt=Add]")
+    .first()
+    .click();
+ 
+   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+   cy.get('[alt="Sylvia Palmer"]').click();
+ 
+   cy.contains("Save").click();
+ 
+   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+   cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
+  it("should edit an interview", () => {
+
+    cy.get('.appointment__card--show').first();
+
+    cy.get("[alt=Edit]").click({ force: true });
   
-});
+    cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Mohamed Thameem");
+
+    cy.get("[alt='Tori Malcolm']").click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Mohamed Thameem");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+ });
